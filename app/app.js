df58c134ca1a2ac2f52c0dab0ad0008bafed8c64
Adriana Villafañe <tsade@live.com.ar>
2023-10-07 22:23:46 -0300

    Empezando con JavaScript

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
 /*  */
+// Botón cierre aside
+
+const closeAsideBtn = document.getElementById("off-btn"); // botón X de cierre aside
+const aside = document.getElementById("aside"); //etiqueta aside
+
+closeAsideBtn.addEventListener("click", () => {
+  aside.classList.add("hidden");//oculta clase
+});
