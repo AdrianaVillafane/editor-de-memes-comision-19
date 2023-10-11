3546538fbd2280ef88791e8b77f61da6454db93e
Adriana Villafañe <tsade@live.com.ar>
2023-10-11 12:00:24 -0300

    Cambio tema funcionando, con cambio de imagen incluido

--- app/app.js
+++ app/app.js
@@ -1,6 +1,26 @@
 
-
+//MODO OSCURO-CLARO// ----->FUNCIONAA
 const $ = (selector) => document.querySelector(selector)
+
+$(".change-theme").addEventListener("click", () => {
+    const currentTheme = $("body").getAttribute("data-mode")
+    if (currentTheme) {
+        $("body").removeAttribute("data-mode", "clear-mode")
+    } else {
+        $("body").setAttribute("data-mode", "clear-mode")
+    }
+    $("#btn-dark-mode").classList.toggle("hidden")
+    $("#btn-clear-mode").classList.toggle("hidden")
+})
+
+//IMAGEN MODO OSCURO-CLARO// ------->
+
+$(".change-theme").addEventListener("click", () => {
+  $(".change-theme").classList.toggle("on")
+})
+
+
+
 
 //COLOR DEL BACKGROUND DEL MEME   ----> FUNCIONA
 $("#color").addEventListener("input", (e) => {
