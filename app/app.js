fea430be50074e74d11eeae4f80ecd946312adde
Adriana Villafañe <tsade@live.com.ar>
2023-10-18 11:39:15 -0300

    text entry

--- app/app.js
+++ app/app.js
@@ -184,5 +184,18 @@ resetFilterBtn.addEventListener("click", (e) => {
 //TEXTO SUPERIOR----->
 
 const txtUpperMeme = document.getElementById("txt-upper-meme")//texto superior
-const topTxtArea = document.getElementById("top-text")//ingreso de texto
+const upperTxtArea = document.getElementById("upper-txt-area")//ingreso de texto
 
+
+const txtLowerMeme = document.getElementById("txt-lower-meme    ")//texto inferior
+const lowerTxtArea = document.getElementById("lower-txt-area")//ingreso texto
+// aca quede
+upperTxtArea.addEventListener("input", () => {
+  let upperTxt = upperTxtArea.value;
+  txtUpperMeme.innerHTML = topTxt;
+});
+
+bottomTxtArea.addEventListener("input", () => {
+  let bottomTxt = bottomTxtArea.value;
+  memeBottomText.innerHTML = bottomTxt;
+});
